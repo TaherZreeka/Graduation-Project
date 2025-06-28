2c5445c194c9bc549a2f4c5c2d6f499f7e1290ff
Taher Zreek <taherazreek@gmail.com>
2025-06-28 13:21:21 +0300

    Edit Blade Admin\dashboard

--- resources/views/pages/admin/profile.blade.php
+++ resources/views/pages/admin/profile.blade.php
@@ -1,7 +1,7 @@
 @extends('pages.admin.admin-content')
 
 @section('content')
-<h2>Profile</h2>
+<h2>الملف الشخصي</h2>
 <div class="shadow-lg p-3 mb-5 mt-3 bg-body-tertiary rounded">
     <div class="container row">
         <div class="col-md-4">
@@ -11,29 +11,29 @@
                         class="card-img-top" alt="Profile Image" style="border-radius: 50%;">
                 </div>
                 <div class="card-body">
-                    <h5 class="card-title">Admin</h5>
-                    <a href="/admin/settings" class="btn btn-outline-warning">Settings</a>
+                    <h5 class="card-title">المشرف</h5>
+                    <a href="/admin/settings" class="btn btn-outline-warning">إعدادات</a>
                 </div>
             </div>
         </div>
         <div class="col-md-8">
             <div class="mb-3">
-                <label for="name" class="form-label">Name</label>
+                <label for="name" class="form-label">الأسم</label>
                 <input type="text" value="{{\App\Models\Admin::first()->name}}" name="name" id="name"
                     class="form-control" readonly>
             </div>
             <div class="mb-3">
-                <label for="email" class="form-label">Email</label>
+                <label for="email" class="form-label">الإيميل</label>
                 <input type="email" value="{{auth()->user()->email}}" name="email" id="email" class="form-control"
                     readonly>
             </div>
             <div class="mb-3">
-                <label for="date_created" class="form-label">Created at</label>
+                <label for="date_created" class="form-label">تم إنشاؤه في</label>
                 <input type="text" value="{{auth()->user()->created_at}}" name="date_created" id="date_created"
                     class="form-control" readonly>
             </div>
             <div class="mb-3">
-                <label for="date_updated" class="form-label">Updated at</label>
+                <label for="date_updated" class="form-label">تم التحديث في</label>
                 <input type="text" value="{{auth()->user()->updated_at}}" name="date_updated" id="date_updated"
                     class="form-control" readonly>
             </div>
--- resources/views/pages/admin/settings.blade.php
+++ resources/views/pages/admin/settings.blade.php
@@ -1,7 +1,7 @@
 @extends('pages.admin.admin-content')
 
 @section('content')
-<h2>Settings</h2>
+<h2>إعدادات</h2>
 
 <!-- Popup messages -->
 @if (session('success'))
@@ -49,32 +49,32 @@
     <form action="/admin/settings" method="post">
         @csrf
         <div class="mb-3">
-            <label for="email" class="form-label">Email</label>
+            <label for="email" class="form-label">الإيميل</label>
             <input type="email" value="{{auth()->user()->email}}" name="email" id="email" class="form-control"
                 aria-describedby="emailHelp" required>
-            <div id="emailHelp" class="form-text">Remember, once you change the email and submit the form, it will
-                automatically log you out.</div>
+            <div id="emailHelp" class="form-text">تذكر، بمجرد تغيير البريد الإلكتروني وإرسال النموذج، سيتم تسجيل خروجك
+                تلقائيًا.</div>
             <x-form-error name="email" />
         </div>
         <div class="mb-3">
-            <label for="old_password" class="form-label">Old Password</label>
+            <label for="old_password" class="form-label">كلمة المرور القديمة</label>
             <input type="password" name="old_password" id="old_password" class="form-control">
             <x-form-error name="old_password" />
         </div>
         <div class="mb-3">
-            <label for="password" class="form-label">New Password</label>
+            <label for="password" class="form-label">كلمة المرور الجديدة</label>
             <input type="password" name="password" id="password" class="form-control">
             <x-form-error name="password" />
         </div>
         <div class="mb-3">
-            <label for="password_confirmation" class="form-label">Re-enter the New Password</label>
+            <label for="password_confirmation" class="form-label">أعد إدخال كلمة المرور الجديدة</label>
             <input type="password" name="password_confirmation" id="password_confirmation" class="form-control">
             <x-form-error name="password_confirmation" />
         </div>
 
         <div class="mb-3">
-            <button type="submit" class="btn btn-primary">Update</button>
-            <button type="reset" class="btn btn-outline-secondary">Clear</button>
+            <button type="submit" class="btn btn-primary">تحديث</button>
+            <button type="reset" class="btn btn-outline-secondary">إلغاء</button>
         </div>
     </form>
 </div>
